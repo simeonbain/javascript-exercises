0825e8dcc63807bcbd02492c8405341c859d57cf
simeonbain <bainsimeon@gmail.com>
2021-11-09 22:10:04 +1100

    Add solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArr, ...elementsToRemove) {
+    const outputArr = []; 
 
+    for (let i = 0; i < inputArr.length; i++) {
+        if (elementsToRemove.includes(inputArr[i]) === false) {
+            outputArr.push(inputArr[i]); 
+        }
+    }
+
+    return outputArr; 
 };
 
 // Do not edit below this line
