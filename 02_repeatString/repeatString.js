8528bb3e9cd629257b839dd8469d5ff532eeabf1
simeonbain <bainsimeon@gmail.com>
2021-11-09 21:12:12 +1100

    Add solution for 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, numRepeats) {
+    if (numRepeats < 0) {
+        return `ERROR`;
+    }
+    
+    let outputString = ``; 
+    for (let i = 0; i < numRepeats; i++) {
+        outputString = outputString + inputString; 
+    }
+    
+    return outputString; 
 };
 
 // Do not edit below this line
