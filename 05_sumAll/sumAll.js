03eb1cbfdc76bde1ed82e25cc061368dc66c6203
simeonbain <bainsimeon@gmail.com>
2021-11-09 22:44:08 +1100

    Add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // Handle unspported inupts by exiting with `ERROR`
+    if (num1 < 0 || 
+        num2 < 0 || 
+        typeof num1 !== `number` || 
+        typeof num2 !== `number`) {
+            
+        return `ERROR`;
+    }
+    
+    // Round so that we are working with ints
+    num1 = Math.round(num1); 
+    num2 = Math.round(num2); 
 
+    // If necessary reorder the nums so that we know num2 is larger
+    if (num1 > num2) {
+        let temp = num1; 
+        num1 = num2; 
+        num2 = temp;
+    }
+
+    // Do the sum
+    let sum = 0; 
+    for (let i = num1; i <= num2; i++) {
+        sum += i; 
+    }
+
+    return sum; 
 };
 
 // Do not edit below this line
