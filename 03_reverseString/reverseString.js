53c4f85180c05c2627642528200be183d4c7dec6
simeonbain <bainsimeon@gmail.com>
2021-11-09 21:33:00 +1100

    Add solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
 const reverseString = function(str) {
+    const inputStrArr = str.split(``); 
+    const outputStrArr = []; 
 
+    for (let i = 0; i < inputStrArr.length; i++) {
+        outputStrArr[inputStrArr.length - 1 - i] = inputStrArr[i]; 
+    }
+    
+    return outputStrArr.join(``); 
 };
 
 // Do not edit below this line
