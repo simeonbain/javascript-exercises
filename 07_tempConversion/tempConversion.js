ad557575f89432ca9160c8301a165126734dd6e3
simeonbain <bainsimeon@gmail.com>
2021-11-09 23:08:31 +1100

    Add solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    temp = (temp - 32) * 5 / 9; // convert to fahrenheit
+    temp = Math.round(temp * 10) / 10; // round to one decimal place 
+    return temp; 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = temp * 9 / 5 + 32; // convert to fahrenheit
+  temp = Math.round(temp * 10) / 10; // round to one decimal place 
+  return temp; 
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
