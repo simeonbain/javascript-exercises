3f7c3601614880d51dfee910a9d220ad40cc468e
simeonbain <bainsimeon@gmail.com>
2021-11-09 23:36:53 +1100

    Add solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) { 
+  return num1 + num2; 
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2; 
 };
 
-const sum = function() {
-	
+const sum = function(arrToSum) {
+  let sum = 0; 
+  for (let i = 0; i < arrToSum.length; i++) {
+    sum += arrToSum[i];
+  }
+
+  return sum; 
 };
 
-const multiply = function() {
+const multiply = function(arrToMultiply) {
+  let product = 1; 
+  for (let i = 0; i < arrToMultiply.length; i++) {
+    product *= arrToMultiply[i];
+  }
 
+  return product; 
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent; 
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	num = Math.round(num); // handle non-integer inputs by rounding
+  
+  let factorial = 1; 
+  for (let i = num; i > 0; i--) {
+    factorial *= i; 
+  }
+
+  return factorial; 
 };
 
 // Do not edit below this line
