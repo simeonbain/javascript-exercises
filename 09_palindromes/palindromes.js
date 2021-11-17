8c96074b3af16c97d4a333f1cc1fb228ecf0546e
simeonbain <bainsimeon@gmail.com>
2021-11-17 22:48:41 +1100

    Add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Convert string to array
+  let array = [...string.toLowerCase()];
 
+  // Remove punctuation
+  array = array.filter((char) => {
+    return !(`,.;:'"/-&! `.includes(char));
+  });
+
+  // Compare to reversed array
+  const reversedArray = array.slice().reverse(); 
+  return array.every((char, index) => char === reversedArray[index]);
+ 
 };
 
 // Do not edit below this line
