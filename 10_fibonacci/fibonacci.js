30b375ab1f9093a31eecd2879161be1610059481
simeonbain <bainsimeon@gmail.com>
2021-11-17 23:02:27 +1100

    Add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n; 
+    if (n < 0) return `OOPS`;
 
+    let fibI = 1; 
+    let fibIMinus1 = 1; 
+    for (let i = 2; i < n; i++) {
+        const temp = fibI;
+        fibI = fibI + fibIMinus1; 
+        fibIMinus1 = temp;
+    }
+    return fibI; 
 };
 
 // Do not edit below this line
